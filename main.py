16e7a0858098b3145699bc16694d3748309515b7
Till Körsmeier <till.koersmeier@t-online.de>
2021-11-11 11:01:35 +0100

    colour

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ file.close()
 
 # Geht Zeile für Zeile durch
 for line in lines:
-    LineObj = funktionen.gcode(line)
-    print("Main: ", LineObj.nr, LineObj.x,LineObj.y, LineObj.z)
-    funktionen.berechnung(LineObj)
+    LineObj = funktionen.gcode(line)    # Schritt 1: G-Code
+    print("Main: ", LineObj.nr,         # Print (überprüfen)
+                    LineObj.x,
+                    LineObj.y,
+                    LineObj.z)
+    funktionen.berechnung(LineObj)      # Schritt 2: Bewegung berechnen
