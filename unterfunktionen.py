9b05f63476d4ef53bef997e35e565e4c44069723
Till Körsmeier <till.koersmeier@t-online.de>
2021-11-21 18:01:55 +0100

    unterfunktion rotate Point

--- unterfunktionen.py
+++ unterfunktionen.py
@@ -1,3 +1,6 @@
+# Importe:
+import math
+
 # Klasse für Line-Objekt erstellen
 class Line:
     def __init__(self, Nr, X, Y, Z):
@@ -34,3 +37,10 @@ def convert_list_to_float(list):
         return i        # return int
     except:
         return e        # return 0
+
+# Punkt-Drehung
+def rotate_point(p, alpha):
+    p_n = [0, 0]
+    p_n[0] = math.cos(alpha) * p[0] - math.sin(alpha) * p[1]
+    p_n[1] = math.sin(alpha) * p[0] + math.cos(alpha) * p[1]
+    return p_n
